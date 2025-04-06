bc8c195347953c45928465b22958f1d894b0b356
ivangarzab <ivangb6@gmail.com>
2025-04-06 18:03:07 -0500

    Create the DataModule class holding a single Koin module field that holds all of the class that we need to inject for the :data module

---
+++ data/src/main/java/com/ivangarzab/data/di/DataModule.kt
@@ -0,0 +1,46 @@
+package com.ivangarzab.data.di
+
+import android.content.Context
+import com.ivangarzab.data.audio.AudioChunksLocalDataSource
+import com.ivangarzab.data.audio.AudioChunksRemoteDataSource
+import com.ivangarzab.data.audio.AudioChunksRepository
+import com.ivangarzab.data.course.CourseLocalDataSource
+import com.ivangarzab.data.course.CourseRemoteDataSource
+import com.ivangarzab.data.course.CourseRepository
+import com.ivangarzab.data.util.JsonLoader
+import com.ivangarzab.data.network.NetworkRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import org.koin.dsl.module
+
+/**
+ * [org.koin.core.Koin] dependency module for injection from the data layer.
+ */
+val dataModule = module {
+    single { CoroutineScope(SupervisorJob() + Dispatchers.IO) }
+    single { JsonLoader(get(Context::class)) }
+    factory { NetworkRepository(get(Context::class)) }
+    // Course data
+    single { CourseRemoteDataSource(get(JsonLoader::class)) }
+    single { CourseLocalDataSource() }
+    single {
+        CourseRepository(
+            get(CourseRemoteDataSource::class),
+            get(CourseLocalDataSource::class),
+            get(NetworkRepository::class),
+            get(CoroutineScope::class)
+        )
+    }
+    // Audio chunks data
+    single { AudioChunksRemoteDataSource(get(JsonLoader::class)) }
+    single { AudioChunksLocalDataSource() }
+    single {
+        AudioChunksRepository(
+            get(AudioChunksRemoteDataSource::class),
+            get(AudioChunksLocalDataSource::class),
+            get(NetworkRepository::class),
+            get(CoroutineScope::class)
+        )
+    }
+}
