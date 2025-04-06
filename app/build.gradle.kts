e8127b6c22f841ef0c51703037fe79891bfe94cb
ivangarzab <ivangb6@gmail.com>
2025-04-06 12:59:58 -0500

    Bring in the :data module into :app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 dependencies {
 
     implementation(project(":resources"))
+    implementation(project(":data"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
